af16c85d859522aa244a5f2c032dbe40040d0637
Will G Piro <willgpiro@gmail.com>
2020-01-14 12:37:25 -0800

    final commit

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,82 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import isYes from '../is-yes.js';
+
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('YesIsEqualtoTrue', function(assert) {
+    const input = 'Yes';
+    const expected = true;
+
+    const result = isYes(input);
+
+    assert.equal(result, expected);
+
+});
+
+test('YIsEqualtoTrue', function(assert) {
+    const input = 'Y';
+    const expected = true;
+
+    const result = isYes(input);
+
+    assert.equal(result, expected);
+
+});
+
+test('YIsEqualtoTrue', function(assert) {
+    const input = 'Ye';
+    const expected = true;
+
+    const result = isYes(input);
+
+    assert.equal(result, expected);
+
+});
+
+test('yIsEqualtoTrue', function(assert) {
+    const input = 'y';
+    const expected = true;
+
+    const result = isYes(input);
+
+    assert.equal(result, expected);
+
+});
+
+test('NoIsEqualtoFalse', function(assert) {
+    const input = 'No';
+    const expected = false;
+
+    const result = isYes(input);
+
+    assert.equal(result, expected);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     //Arrange
     // Set up your parameters and expectations
-
+ 
     //Act 
     // Call the function you're testing and set the result to a const
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
-});
+  
+
